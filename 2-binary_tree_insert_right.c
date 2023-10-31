d00da860c9c052e45022ccc5a927884d326f6c2f
ahmedehab0 <ahmed.eh01@gmail.com>
2023-10-31 19:54:32 -0400

    2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_insert_left - function that inserts a node as
+ *the left child of another node
+ *
+ *@parent: pointer to the parent node of the node to create
+ *@value: value to put in the new node
+ *Return: pointer to the new node, or NULL on faliure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	int right;
+
+	if (parent)
+	{
+		if (parent->right)
+		{
+			right = (parent->right)->n;
+			parent->right = binary_tree_node(parent, value);
+			parent->right->right = binary_tree_node(parent->right, right);
+		}
+		else
+		{
+			parent->right = binary_tree_node(parent, value);
+		}
+		return (parent->right);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
